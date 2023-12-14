ed2704a559b79bd49c32087c75bd7a759470b93d
Darley Leal <darleyleal98@gmail.com>
2023-12-14 19:00:28 -0300

    Criado Controllers do projeto, conexão com o postgre (deve efetuar o update-database depois para criar a tabela) ela chamando a tela inicial de Login (aparece a verde escuro padrão que criamos). A documentação também já está anexada ao projeto.

---
+++ sistema-acessidoc/Models/Usuario/UsuarioCapModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaAcessiDoc.Models
+{
+    [Table("usuarios")]
+    public class UsuarioCapModel
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+
+        public string Nome { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "O campo de e-mail é obrigatório.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "O campo senha é obrigatório.")]
+        public string Senha { get; set; }
+    }
+}
