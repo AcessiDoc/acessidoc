0250cb4801d458ce648651fd3829e33e63cda268
Darley Leal <darleyleal98@gmail.com>
2023-12-14 17:58:17 -0300

    Adicionar arquivos de projeto.

---
+++ sistema-acessidoc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sistema_acessidoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
